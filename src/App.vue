5a9179b1f355f71d0c879eb6e1b527611e758c2d
Raoul Mondor <2355294@csfoy.ca>
2025-02-18 08:50:37 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import Timeline from './components/Timeline.vue'
+</script>
+
+<template>
+  <Timeline />
+</template>
+
+<style scoped></style>
